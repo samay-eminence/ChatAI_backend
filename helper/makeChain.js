87f0362b311d05a629fcbaf0c65973f1773a22f1
samay-eminence <135198311+samay-eminence@users.noreply.github.com>
2023-06-08 15:24:08 +0530

    Update makeChain.js

--- helper/makeChain.js
+++ helper/makeChain.js
@@ -8,7 +8,7 @@ Chat History:
 Follow-up Question: {question}
 Rephrased Standalone Question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are a helpful AI assistant. Use the following context to answer the question at the end.
 If you don't know the answer, just say you don't know.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
